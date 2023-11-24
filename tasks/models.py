8e4db4bdd01c49a002a62aef509eddd742517b87
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-24 16:27:54 -0300

    feat: person and task models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+
+    def __str__(self) -> str:
+        return self.name
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    role = models.IntegerField(choices=((1, ("Cliente")),
+                                        (2, ("Colaborador"))),
+                                default=1)
+
+
+
+
+class Task(models.Model):
+
+    def __str__(self) -> str:
+        return self.title
+
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    status = models.BooleanField(default=False)
+    person = models.ManyToManyField(Person)
