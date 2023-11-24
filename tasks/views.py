d4d4533fd55d6030e712cf20c2c29255db3f8a94
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-24 18:10:31 -0300

    feat: getbyid to template page works

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Task, Person
 from django.shortcuts import render
 
 
 def getTasks(request):
     taskList = Task.objects.all()
-    return render(request, 'task_manager/index.html', {'taskList': taskList})
+    return render(request, 'tasks/index.html', {'taskList': taskList})
 
 def getById(request, task_id):
-    return HttpResponse(f"Getting task {task_id}")
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("a task que voce procura não existe")
+    return render(request, 'tasks/taskDetail.html', {"task": task})
