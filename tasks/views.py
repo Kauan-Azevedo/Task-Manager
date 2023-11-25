de1a5a8a3bc096e31e53260291b62e6b014fb24c
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-25 00:54:06 -0300

    feat: create task added

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from .models import Task, Person
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def getTasks(request):
@@ -13,3 +13,28 @@ def getById(request, task_id):
     except Task.DoesNotExist:
         raise Http404("a task que voce procura não existe")
     return render(request, 'tasks/taskDetail.html', {"task": task})
+
+def taskAdd(request):
+    person = Person.objects.all()
+
+    if request.method == "POST":
+        data = request.POST
+        title = data.get("title")
+        description = data.get("description")
+        status = data.get("status")
+        person_names = request.POST.getlist("person")
+
+        new_task = Task.objects.create(
+            title=title,
+            description=description,
+            status=status,
+        )
+
+        for person_name in person_names:
+            person_instance = Person.objects.get(name=person_name)
+            new_task.person.add(person_instance)
+        
+        return redirect("tasks:taskDetail", task_id=new_task.id)
+
+
+    return render(request, 'tasks/taskAdd.html', {"person": person, "status": {0: "Pendente", 1: "Concluida"}})
