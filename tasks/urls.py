31de2ea30137c984a7fb7302282e6f3cdf5610e0
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-25 04:08:51 -0300

    feat: complete logic of Update and Delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,7 @@ app_name = 'tasks'
 urlpatterns = [
     path("", views.getTasks, name="taskList"),
     path("task/<int:task_id>", views.getById, name="taskDetail"),
-    path("add/task/", views.taskAdd, name="taskAdd"),
+    path("task/add/", views.taskAdd, name="taskAdd"),
+    path("task/<int:task_id>/update/", views.taskUpdate, name="taskUpdate"),
+    path("task/<int:task_id>/delete/", views.taskDelete, name="taskDelete")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -34,7 +34,44 @@ def taskAdd(request):
             person_instance = Person.objects.get(name=person_name)
             new_task.person.add(person_instance)
         
+        new_task.save()
         return redirect("tasks:taskDetail", task_id=new_task.id)
 
 
     return render(request, 'tasks/taskAdd.html', {"person": person, "status": {0: "Pendente", 1: "Concluida"}})
+
+def taskUpdate(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Tarefa inexistente!")
+
+    if request.method == "POST":
+        data = request.POST
+        task.title = data.get("title")
+        task.description = data.get("description")
+        task.status = data.get("status")
+        task.person.clear()
+        
+        person_names = request.POST.getlist("person")
+        for person_name in person_names:
+            person_instance = Person.objects.get(name=person_name)
+            task.person.add(person_instance)
+        
+        task.save()
+        return redirect("tasks:taskDetail", task_id=task.id)
+
+    person = Person.objects.all()
+    return render(request, 'tasks/taskUpdate.html', {"task": task, "person": person, "status": {0: "Pendente", 1: "Concluída"}})
+
+def taskDelete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Tarefa inexistente!")
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("tasks:listTasks")
+
+    return render(request, 'tasks/taskDelete.html', {"task": task})
