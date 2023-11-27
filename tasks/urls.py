f41529c1fc7f7710c5eafed324be89e8bde5cdc2
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-27 16:46:09 -0300

    feat: person html

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,8 +6,15 @@ app_name = 'tasks'
 
 urlpatterns = [
     path("", views.getTasks, name="taskList"),
-    path("task/<int:task_id>", views.getById, name="taskDetail"),
+    path("task/<int:task_id>", views.getTaskById, name="taskDetail"),
     path("task/add/", views.taskAdd, name="taskAdd"),
     path("task/<int:task_id>/update/", views.taskUpdate, name="taskUpdate"),
-    path("task/<int:task_id>/delete/", views.taskDelete, name="taskDelete")
+    path("task/<int:task_id>/delete/", views.taskDelete, name="taskDelete"),
+
+    path("person/", views.getPersons, name="personList"),
+    path("person/<int:person_id>", views.getPersonById, name="personDetail"),
+    path("person/add/", views.personAdd, name="personAdd"),
+    path("person/<int:person_id>/update/", views.personUpdate, name="personUpdate"),
+    path("person/<int:person_id>/delete/", views.personDelete, name="personDelete"),
+
 ]
